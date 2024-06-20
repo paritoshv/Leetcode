fcac536b74af9bc645cea3ad9baae22980dc0c20
Paritosh Varshney <56546034+paritoshv@users.noreply.github.com>
2024-06-20 18:10:55 +0530

    Time: 396 ms (16.08%) | Memory: 122.2 MB (39.10%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     int minimumCardPickup(vector<int>& cards) {
-        // int i =0;
-        // int j =1;
-        // int ans =cards.size()+1;
-        // while(i<cards.size() && j< cards.size()){
-        //     while(j<cards.size() && cards[i]!=cards[j] ){
-        //         j++;
-        //     }
-        //     if(j<cards.size() && cards[i]==cards[j])
-        //     ans = min(ans, j-i+1);
-        //     i++;
-        //     j=i+1;
-        // }
-
-        // if(ans == cards.size()+1){
-        //     return -1;
-        // }
-
-        // return ans;
         map<int,int> store;
         int ans = cards.size()+1;
         for(int i =0; i<cards.size();i++){
@@ -32,9 +14,6 @@ public:
         if(ans == cards.size()+1){
             return -1;
         }
-
         return ans;
-
-
     }
 };
