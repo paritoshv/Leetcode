fd1bb73384045bd014ebac3798703410794ab75b
Paritosh Varshney <56546034+paritoshv@users.noreply.github.com>
2024-07-24 17:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (9.94%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int reverse(int x) {
-        double k =0;
+        int k =0;
         int p = abs(x);
         while(p>0){
+            if(k>INT_MAX/10 || k<INT_MIN/10) return 0;
             int m =p%10;
             k = k*10 + m;
             p=p/10;
         }
         cout << k;
-        if((x > 0 ? k > (pow(2,31) -1) : false) || (x < 0 ? (k > pow(2,31)) : false)){   
-          return 0;  
-        }
-        else {
+
             return x > 0 ? k : (-1)* k;
-        }
+
         
     }
 };
