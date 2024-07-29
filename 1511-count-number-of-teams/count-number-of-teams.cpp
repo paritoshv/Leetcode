13d9d1d616e6c2bb4f3c5d9eb40cffc8dbb8cc26
Paritosh Varshney <56546034+paritoshv@users.noreply.github.com>
2024-07-29 12:35:03 +0530

    Time: 30 ms (70.71%) | Memory: 11.7 MB (51.74%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-        int cnt =0;
-        for(int i=0;i<rating.size()-2;i++){
-            for(int j =i+1; j<rating.size()-1;j++){
-                for(int k =j+1; k<rating.size();k++){
-                    if((rating[i] < rating[j] && rating[j] <rating[k]) || 
-                    (rating[i] > rating[j] && rating[j] > rating[k])){
-                        cnt++;
-                    }
-                }  
-            }
+        const int n = rating.size();
+        int cnt= 0;
+        for (int i= 1; i < n-1; i++) {
+            int L[2]={0}, R[2]={0};
+
+            // Count ratings on the left of i
+            for (int j = 0; j < i; j++) 
+                L[rating[j] < rating[i]]++;
+            
+            // Count ratings on the right of i
+            for (int k=i+1; k<n; k++) 
+                R[rating[k] < rating[i]]++;
+
+            // number of valid teams
+            cnt += L[0]*R[1] + L[1]*R[0];
         }
         return cnt;
-        
     }
 };
