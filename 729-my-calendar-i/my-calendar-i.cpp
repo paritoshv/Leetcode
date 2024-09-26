fabc189647cdf3cd342a87cf56e7ebd7166c6bad
Paritosh Varshney <56546034+paritoshv@users.noreply.github.com>
2024-09-26 12:27:27 +0530

    Time: 74 ms (74.00%) | Memory: 41.6 MB (86.63%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,23 @@
+class MyCalendar {
+private:
+    vector<pair<int,int>> calendar;
+public:
+    MyCalendar() {
+    }
+    
+    bool book(int start, int end) {
+        for(auto [s,e] : calendar){
+            if(start<e && s<end){
+                return false;
+            }
+        }
+        calendar.push_back(make_pair(start,end));
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
